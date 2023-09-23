fd32fd5c0923a24c6d9bfeb13972e0a1255f4534
khaled308 <khaledgaballa4@gmail.com>
2023-09-23 18:58:21 +0300

    setup swagger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+import specs from "./swaggerConfig";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world!");
-});
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 export default app;
